454a830d1e21da29c55dc6bb9325ea0dedc0fa97
Mark Conroy <mark@annertech.com>
2023-05-03 21:52:48 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const playButton = document.querySelector(".reply-button");
+
+const sounds = [
+  "replies/bye.m4a",
+  "replies/glad-you-enjoyed-it.m4a",
+  "replies/no-problem.m4a",
+  "replies/ok.m4a",
+  "replies/thank-you.m4a",
+  "replies/thanks-you-too.m4a",
+  "replies/youre-very-welcome.m4a",
+];
+
+function playSound() {
+  const sound = new Audio(sounds[Math.floor(Math.random() * sounds.length)]);
+  sound.play();
+}
+
+playButton.addEventListener("click", playSound);
